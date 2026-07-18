d9d781c21c8c36e218471e7345699ea40fff0d23
subhankar das <dassubhankardassubhankar@gmail.com>
2026-07-18 19:12:47 +0530

    Product Section added

---
+++ src/main/java/com/project/model/Product.java
@@ -0,0 +1,37 @@
+package com.project.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private String productUrl;
+    private Integer stockQuantity;
+    private BigDecimal price;
+    @CreationTimestamp
+    private LocalDateTime createAt;
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+    private boolean active=true;
+
+}
