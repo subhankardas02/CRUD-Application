5257ff4231a7a1044db11d6f59450ee7635c186b
subhankar das <dassubhankardassubhankar@gmail.com>
2026-07-24 23:39:58 +0530

    ssd

--- src/main/java/com/project/repository/CartItemRepository.java
+++ src/main/java/com/project/repository/CartItemRepository.java
@@ -1,9 +1,12 @@
 package com.project.repository;
 
 import com.project.model.CartItem;
+import com.project.model.Product;
+import com.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByUserAndProduct(User user, Product product);
 }
--- src/main/java/com/project/service/CartService.java
+++ src/main/java/com/project/service/CartService.java
@@ -2,25 +2,43 @@ package com.project.service;
 
 import com.project.dto.CartItemRequest;
 import com.project.dto.ProductResponse;
+import com.project.model.CartItem;
 import com.project.model.Product;
+import com.project.model.User;
 import com.project.repository.CartItemRepository;
+import com.project.repository.ProductRepository;
 import com.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
 public class CartService {
     @Autowired
-    private ProductResponse productResponse;
+    private ProductRepository productRepository;
     @Autowired
     private CartItemRepository cartItemRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
 
-    public void addToCart(String userId, CartItemRequest request) {
-        Optional<Product> productOpt=userRepository.findById(request.getProductId());
+    public boolean addToCart(String userId, CartItemRequest request) {
+        Optional<Product> productOpt= productRepository.findById(request.getProductId());
+        if(productOpt.isEmpty()) return false;
+        Product product=productOpt.get();
+        if(product.getStockQuantity()<request.getQuantity()) return false;
+        Optional<User> userOpt=userRepository.findById(Long.valueOf(userId));
+        if(userOpt.isEmpty()) return false;
+        User user=userOpt.get();
+        CartItem existingCartItem=cartItemRepository.findByUserAndProduct(user, product);
+        if(existingCartItem!=null){
+            existingCartItem.setQuantity(existingCartItem.getQuantity()+ request.getQuantity());
+            existingCartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(existingCartItem.getQuantity())));
+            cartItemRepository.save(existingCartItem);
+        }
+
 
 
     }
